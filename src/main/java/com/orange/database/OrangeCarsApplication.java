33dac42bacc4c90fdd8e7aec13d53d66f0489701
EricaSugui <ericasugui@gmail.com>
2021-05-22 23:42:38 -0300

    adding springboot project

---
+++ src/main/java/com/orange/database/OrangeCarsApplication.java
@@ -0,0 +1,13 @@
+package com.orange.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/database/OrangeCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
